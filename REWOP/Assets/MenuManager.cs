de53111e8ce198a4d895bb8c9151a5a4f310f2a5
Louie <Louie>
2018-01-17 15:13:47 +0800

    Added Game Setting
    -Quality Setting will be implement ASAP
    -Attack sound effect will be implement ASAP
    -In game sound setting & quality setting will be implement ASAP

---
+++ REWOP/Assets/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject mainMenuHolder;
+	public GameObject optionsMenuHolder;
+
+	public Slider[] volumeSliders;
+
+	void start ()
+	{
+		volumeSliders [0].value = AudioManager.instance.masterVolumePercent;
+
+
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+	public void OptionsMenu()
+	{
+		mainMenuHolder.SetActive (false);
+		optionsMenuHolder.SetActive (true);
+	}
+
+	public void MainMenu ()
+	{
+		mainMenuHolder.SetActive (true);
+		optionsMenuHolder.SetActive (false);
+	}
+
+	public void MasterVolume(float value)
+	{
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Master);
+
+
+	}
+}

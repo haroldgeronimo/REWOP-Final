db7e10e48eaf571b54d7c11eba284a84e7b22110
Louie <Louie>
2018-02-06 02:57:59 +0800

    Map (uncomplete)

---
+++ REWOP/Assets/NoRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotate : MonoBehaviour {
+
+	public Transform playerTransform;
+	public float offset_y = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 eulerAngeles = transform.eulerAngles;
+		eulerAngeles.y = 0;
+		eulerAngeles.z = 0;
+		transform.eulerAngles = eulerAngeles;
+	}
+}
